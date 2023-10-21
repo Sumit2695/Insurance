5a7018f6cc9c4436479a2eb480f72f1728314c7d
Sumit <sumit.sjoshi2695@gmail.com>
2023-10-21 20:46:25 +0530

    S123

---
+++ src/main/java/BaseClass.java
@@ -0,0 +1,24 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	FileInputStream fileInputStream;
+	public WebDriver driver;
+
+	public void LaunchTheWeb() throws IOException{
+		 fileInputStream = new FileInputStream(".\\src\\main\\resources\\property");
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Properties properties=new Properties();
+		properties.getProperty("weburl");
+		driver.get(properties.getProperty("weburll"));
+		
+
+
+}
+}
